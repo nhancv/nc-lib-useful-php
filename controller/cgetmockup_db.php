2c6a58a2458d101eb179a582952c72022d93a38a
Nhan Cao <caovannhan2002@gmail.com>
2016-02-01 20:42:52 +0530

    mockup data

---
+++ controller/cgetmockup_db.php
@@ -0,0 +1,27 @@
+<?php  
+header("Content-Type: text/html;charset=UTF-8");
+// array for JSON response
+$response = array();
+require_once dirname(__FILE__) . '/function/json_encode_utf8.php';
+require_once dirname(__FILE__) . '/controller.php';
+
+if(isset($_GET["u"])) {
+    $username=$_GET["u"];
+}else if(isset($_POST["u"])) {
+    $username=$_POST["u"];
+}
+
+if(isset($_GET["p"])) {
+    $password=$_GET["p"];
+}else if(isset($_POST["p"])) {
+    $password=$_POST["p"];
+}
+if (isset($username) && isset($password)) {
+    $file = file_get_contents("../model/localdb/data.json");
+    print($file);
+}else {
+    $response["message"] = "Required field(s) is missing: url (required)";
+    echo json_encode($response);
+}
+
+?>
